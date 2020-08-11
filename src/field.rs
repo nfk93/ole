a240724c74833d009e2822441d5f7cbefe9bc1e8
nfk93 <nfk1993@gmail.com>
2020-08-12 01:16:13 +0200

    euclidean division

--- src/field.rs
+++ src/field.rs
@@ -252,12 +252,114 @@ fn horner(coefficients: &[Fp], variable: &Fp) -> Fp {
     })
 }
 
+// input: polynomials a and b in coefficient representation
+// outputs: (q, r) such that q*b + r = a, with deg(r) < deg(b)
+// precondition: leading coefficient of a and b (i.e. a[a.len() -1]) is nonzero
+fn euclid_division(a: &[Fp], b: &[Fp]) -> (Vec<Fp>, Vec<Fp>) {
+    let mut deg_r = a.len() - 1;
+    let mut q = vec!(Fp::zero(); deg_r + 1);
+    let mut r = a.to_vec();
+    let d = b.len() - 1; // degree of b
+    let c = b[d].inverse().unwrap(); // c = lc(b)^-1
+    while deg_r >= d {
+        let mut s = r[deg_r]; // s = lc(r)
+        s.mul_assign(&c); // s = lc(r)/lc(b)
+        q[deg_r - d].add_assign(&s);
+        for i in 0..(d + 1) {
+            let mut sb_i = s;
+            sb_i.mul_assign(&b[i]);
+            r[deg_r - d + i].sub_assign(&sb_i);
+        }
+        'update_deg_r: while true {
+            if r[deg_r].is_zero() {
+                deg_r -= 1;
+            }
+            else {
+                r.truncate(deg_r + 1);
+                break 'update_deg_r;
+            }
+        }
+    }
+    return (q, r)
+}
+
+fn naive_poly_mult(a: &[Fp], b: &[Fp]) -> Vec<Fp> {
+    let mut prod = vec!(Fp::zero(); a.len() + b.len() - 1);
+
+    for i in 0..a.len() {
+       for j in 0..b.len() {
+           let mut ab = a[i];
+           ab.mul_assign(&b[j]);
+           prod[i+j].add_assign(&ab);
+       }
+    }
+
+    return prod
+}
+
+// adds polynomial b to polynomial a.
+// precondition deg(a) > deg(b)
+fn poly_add(a: &mut [Fp], b: &[Fp]) {
+    for (idx, coeff) in b.iter().enumerate() {
+        a[idx].add_assign(coeff);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use rand;
 
-    // const P_MINUS_ONE_FACTORS: [u64; 4] = [2, 3, 23, 54697345034152330060240659727];
+    #[test]
+    fn test_poly_add() {
+        let mut rng = rand::thread_rng();
+        let a: Vec<Fp> = (0..101).map(|_| Fp::random(&mut rng)).collect();
+        let b: Vec<Fp> = (0..22).map(|_| Fp::random(&mut rng)).collect();
+        let mut c = a.clone();
+        poly_add(&mut c, &b);
+        for _ in 0..1000 {
+            let point = Fp::random(&mut rng);
+            let mut a_ = horner(&a, &point);
+            let b_ = horner(&b, &point);
+            let c_ = horner(&c, &point);
+
+            a_.add_assign(&b_);
+            assert_eq!(a_, c_);
+        }
+    }
+
+    #[test]
+    fn test_naive_poly_mult() {
+        let mut rng = rand::thread_rng();
+        let a: Vec<Fp> = (0..101).map(|_| Fp::random(&mut rng)).collect();
+        let b: Vec<Fp> = (0..22).map(|_| Fp::random(&mut rng)).collect();
+        let prod = naive_poly_mult(&a, &b);
+        for _ in 0..1000 {
+            let point = Fp::random(&mut rng);
+            let mut a_ = horner(&a, &point);
+            let b_ = horner(&b, &point);
+            let c_ = horner(&prod, &point);
+
+            a_.mul_assign(&b_);
+            assert_eq!(a_, c_);
+        }
+    }
+
+    #[test]
+    fn test_euclid_division() {
+        let mut rng = rand::thread_rng();
+        let a: Vec<Fp> = (0..83).map(|_| Fp::random(&mut rng)).collect();
+        let b: Vec<Fp> = (0..22).map(|_| Fp::random(&mut rng)).collect();
+        let (q,r) = euclid_division(&a, &b);
+        let mut actual = naive_poly_mult(&q, &b);
+        println!("{:?}", q);
+        println!("{:?}", r);
+        poly_add(&mut actual, &r);
+
+        for (idx, coeff) in a.iter().enumerate() {
+            assert_eq!(actual[idx], *coeff);
+        }
+    }
 
     #[test]
     fn test_horner() {
